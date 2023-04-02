9f6419dbd47dbe992e738c9faa881de77b0063bb
Shaxboz <shshwopiwppu29082007@gmail.com>
2023-04-02 14:41:56 +0500

    views.py

--- myapp/views.py
+++ myapp/views.py
@@ -6,8 +6,10 @@ from .models import *
 def HomePage(request):
     context = InfoModell.objects.all()
     return render(request,'blog.html',{'context':context})
+    return
 
 def aboutpage(request,name):
     obj = InfoModell.objects.get(name = name)
     Name = PhoneModel.objects.all()
     return render(request,'detail.html',{'obj':obj,"Name":Name})
+
